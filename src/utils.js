8d442ebd11c43a3bd6d9658d67ada0e3a3220899
m26unkwn <getmonushukla@gmail.com>
2022-07-10 23:48:17 +0530

    eslint: error resolve

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 export const filterTags = (todoList, tags) => {
   const allTodo = [...todoList];
   if (tags.length > 0) {
+    // eslint-disable-next-line
     return allTodo.filter((todoItem) => {
       for (let index = 0; index < todoItem.tags.length; index++) {
         if (tags.includes(todoItem.tags[index])) {
